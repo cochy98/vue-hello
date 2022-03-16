869e9e2d21d3dc46c04500c0599cd493fab9a6d3
cochy98 <cosmoferrigno@gmail.com>
2022-03-16 16:56:43 +0100

    prova contenuti dinamici con Vue

--- js/main.js
+++ js/main.js
@@ -5,9 +5,15 @@
     Aggiungere alla pagina un’immagine, presa anch’essa da un data, magari volendo sperimentare anche prendendola da un input.
  */
 
+// Mi creo una nuova istanza dell oggetto 'Vue'
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello World!'
+        message: '',
+        outputText: 'Questa è la prova di un titolo in Vue.',
+        image: {
+            name: 'Vue logo',
+            source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/555px-Vue.js_Logo_2.svg.png'
+        }
     }
 });
